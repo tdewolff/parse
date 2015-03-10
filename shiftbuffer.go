30202d6c495ebf3f3443357ec8b83eba581eb7c4
Taco de Wolff <tacodewolff@gmail.com>
2015-03-10 18:38:28 +0100

    Slightly faster

--- shiftbuffer.go
+++ shiftbuffer.go
@@ -21,7 +21,7 @@ type ShiftBuffer struct {
 
 	buf []byte
 	pos int
-	n   int
+	end int
 }
 
 // NewShiftBufferReader returns a new ShiftBuffer for a given io.Reader.
@@ -45,7 +45,7 @@ func NewShiftBuffer(r io.Reader) *ShiftBuffer {
 
 // Err returns the error.
 func (z *ShiftBuffer) Err() error {
-	if z.err == io.EOF && z.pos+z.n < len(z.buf) {
+	if z.err == io.EOF && z.end < len(z.buf) {
 		return nil
 	}
 	return z.err
@@ -58,17 +58,17 @@ func (z *ShiftBuffer) IsEOF() bool {
 
 // Move advances the 0 position of read.
 func (z *ShiftBuffer) Move(n int) {
-	z.n += n
+	z.end += n
 }
 
 // MoveTo sets the 0 position of read.
 func (z *ShiftBuffer) MoveTo(n int) {
-	z.n = n
+	z.end = z.pos + n
 }
 
 // Pos returns the 0 position of read.
 func (z *ShiftBuffer) Pos() int {
-	return z.n
+	return z.end - z.pos
 }
 
 // Len returns the length of the buffer.
@@ -78,14 +78,15 @@ func (z *ShiftBuffer) Len() int {
 
 // Peek returns the ith byte and possible does a reallocation
 func (z *ShiftBuffer) Peek(i int) byte {
-	if z.pos+z.n+i >= len(z.buf) {
+	end := z.end+i
+	if end >= len(z.buf) {
 		if z.err != nil {
 			return 0
 		}
 
 		// reallocate a new buffer (possibly larger)
 		c := cap(z.buf)
-		d := z.n + i
+		d := end - z.pos
 		var buf1 []byte
 		if 2*d > c {
 			if 2*c > MaxBuf {
@@ -96,17 +97,18 @@ func (z *ShiftBuffer) Peek(i int) byte {
 		} else {
 			buf1 = z.buf[:d]
 		}
-		copy(buf1, z.buf[z.pos:z.pos+d])
+		copy(buf1, z.buf[z.pos:end])
 
 		// Read in to fill the buffer till capacity
 		var n int
 		n, z.err = z.r.Read(buf1[d:cap(buf1)])
+		z.end -= z.pos
 		z.pos, z.buf = 0, buf1[:d+n]
 		if n == 0 {
 			return 0
 		}
 	}
-	return z.buf[z.pos+z.n+i]
+	return z.buf[end]
 }
 
 // PeekRune returns the rune of the ith byte.
@@ -126,13 +128,12 @@ func (z *ShiftBuffer) PeekRune(i int) rune {
 
 // Buffered returns the bytes of the current selection.
 func (z *ShiftBuffer) Buffered() []byte {
-	return z.buf[z.pos : z.pos+z.n]
+	return z.buf[z.pos : z.end]
 }
 
 // Shift returns the bytes of the current selection and advances the position.
 func (z *ShiftBuffer) Shift() []byte {
-	b := z.buf[z.pos : z.pos+z.n]
-	z.pos += z.n
-	z.n = 0
+	b := z.buf[z.pos : z.end]
+	z.pos = z.end
 	return b
 }
