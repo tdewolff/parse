a72310d7f6cc8632e86f5edcd28c5c913b4670e9
Taco de Wolff <tacodewolff@gmail.com>
2019-11-27 16:47:12 -0300

    Re-parse file immediately on error to prevent displaying the wrong error if the underlying byte slice is modified, partially fixes gohugoio/hugo#6427 and #53

--- error.go
+++ error.go
@@ -10,19 +10,19 @@ import (
 // Error is a parsing error returned by parser. It contains a message and an offset at which the error occurred.
 type Error struct {
 	Message string
-	r       io.Reader
-	Offset  int
-	line    int
-	column  int
-	context string
+	Line    int
+	Column  int
+	Context string
 }
 
 // NewError creates a new error
 func NewError(msg string, r io.Reader, offset int) *Error {
+	line, column, context := Position(r, offset)
 	return &Error{
 		Message: msg,
-		r:       r,
-		Offset:  offset,
+		Line:    line,
+		Column:  column,
+		Context: context,
 	}
 }
 
@@ -33,17 +33,13 @@ func NewErrorLexer(msg string, l *buffer.Lexer) *Error {
 	return NewError(msg, r, offset)
 }
 
-// Positions re-parses the file to determine the line, column, and context of the error.
+// Positions returns the line, column, and context of the error.
 // Context is the entire line at which the error occurred.
 func (e *Error) Position() (int, int, string) {
-	if e.line == 0 {
-		e.line, e.column, e.context = Position(e.r, e.Offset)
-	}
-	return e.line, e.column, e.context
+	return e.Line, e.Column, e.Context
 }
 
 // Error returns the error string, containing the context and line + column number.
 func (e *Error) Error() string {
-	line, column, context := e.Position()
-	return fmt.Sprintf("parse error:%d:%d: %s\n%s", line, column, e.Message, context)
+	return fmt.Sprintf("parse error: %s on line %d and column %d\n%s", e.Message, e.Line, e.Column, e.Context)
 }
