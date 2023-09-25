5c3ffa29a65fb304f8d81634d523641c68b7bba8
Taco de Wolff <tacodewolff@gmail.com>
2023-09-24 21:35:20 -0300

    Remove error prefix for some lexers/parsers

--- css/parse.go
+++ css/parse.go
@@ -216,7 +216,7 @@ func (p *Parser) parseDeclarationList() GrammarType {
 	// parse error
 	p.initBuf()
 	p.l.r.Move(-len(p.data))
-	p.err, p.errPos = fmt.Sprintf("CSS parse error: unexpected token '%s' in declaration", string(p.data)), p.l.r.Offset()
+	p.err, p.errPos = fmt.Sprintf("unexpected token '%s' in declaration", string(p.data)), p.l.r.Offset()
 	p.l.r.Move(len(p.data))
 
 	if p.tt == RightBraceToken {
@@ -266,7 +266,7 @@ func (p *Parser) parseAtRule() GrammarType {
 				if 1 < len(p.state) {
 					p.state = p.state[:len(p.state)-1]
 				}
-				p.err, p.errPos = "CSS parse error: unexpected ending in at rule", p.l.r.Offset()
+				p.err, p.errPos = "unexpected ending in at rule", p.l.r.Offset()
 				return ErrorGrammar
 			}
 			p.level--
@@ -348,7 +348,7 @@ func (p *Parser) parseQualifiedRule() GrammarType {
 			p.state = append(p.state, (*Parser).parseQualifiedRuleDeclarationList)
 			return BeginRulesetGrammar
 		} else if tt == ErrorToken {
-			p.err, p.errPos = "CSS parse error: unexpected ending in qualified rule", p.l.r.Offset()
+			p.err, p.errPos = "unexpected ending in qualified rule", p.l.r.Offset()
 			return ErrorGrammar
 		} else if tt == LeftParenthesisToken || tt == LeftBraceToken || tt == LeftBracketToken || tt == FunctionToken {
 			p.level++
@@ -359,7 +359,7 @@ func (p *Parser) parseQualifiedRule() GrammarType {
 				if 1 < len(p.state) {
 					p.state = p.state[:len(p.state)-1]
 				}
-				p.err, p.errPos = "CSS parse error: unexpected ending in qualified rule", p.l.r.Offset()
+				p.err, p.errPos = "unexpected ending in qualified rule", p.l.r.Offset()
 				return ErrorGrammar
 			}
 			p.level--
@@ -402,7 +402,7 @@ func (p *Parser) parseDeclaration() GrammarType {
 	tt, data := p.popToken(false)
 	if tt != ColonToken {
 		p.l.r.Move(-len(data))
-		p.err, p.errPos = "CSS parse error: expected colon in declaration", p.l.r.Offset()
+		p.err, p.errPos = "expected colon in declaration", p.l.r.Offset()
 		p.l.r.Move(len(data))
 		p.pushBuf(ttName, dataName)
 		return p.parseDeclarationError(tt, data)
@@ -419,7 +419,7 @@ func (p *Parser) parseDeclaration() GrammarType {
 		} else if tt == RightParenthesisToken || tt == RightBraceToken || tt == RightBracketToken {
 			if p.level == 0 {
 				// TODO: buggy
-				p.err, p.errPos = "CSS parse error: unexpected ending in declaration", p.l.r.Offset()
+				p.err, p.errPos = "unexpected ending in declaration", p.l.r.Offset()
 				p.pushBuf(ttName, dataName)
 				p.pushBuf(ColonToken, []byte{':'})
 				return p.parseDeclarationError(tt, data)
@@ -466,7 +466,7 @@ func (p *Parser) parseCustomProperty() GrammarType {
 	p.initBuf()
 	if tt, data := p.popToken(false); tt != ColonToken {
 		p.l.r.Move(-len(data))
-		p.err, p.errPos = "CSS parse error: expected colon in custom property", p.l.r.Offset()
+		p.err, p.errPos = "expected colon in custom property", p.l.r.Offset()
 		p.l.r.Move(len(data))
 		return ErrorGrammar
 	}
@@ -483,7 +483,7 @@ func (p *Parser) parseCustomProperty() GrammarType {
 			if p.level == 0 {
 				// TODO: buggy
 				p.pushBuf(tt, data)
-				p.err, p.errPos = "CSS parse error: unexpected ending in custom property", p.l.r.Offset()
+				p.err, p.errPos = "unexpected ending in custom property", p.l.r.Offset()
 				return ErrorGrammar
 			}
 			p.level--
