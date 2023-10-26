a5bbb11e79fe5b3ef1059649f3009e19c2e12435
Taco de Wolff <tacodewolff@gmail.com>
2023-10-25 21:10:30 -0300

    Add JSString fuzzing, see #108 and #109

---
+++ tests/js-string/main.go
@@ -0,0 +1,34 @@
+// +build gofuzz
+
+package fuzz
+
+import (
+	"fmt"
+	"unicode/utf8"
+
+	"github.com/tdewolff/parse/v2"
+	"github.com/tdewolff/parse/v2/js"
+)
+
+// Fuzz is a fuzz test.
+func Fuzz(data []byte) int {
+	if !utf8.Valid(data) {
+		return 0
+	}
+
+	o := js.Options{}
+	input := parse.NewInputBytes(data)
+	if ast, err := js.Parse(input, o); err == nil {
+		src := ast.JSString()
+		input2 := parse.NewInputString(src)
+		if ast2, err := js.Parse(input2, o); err != nil {
+			panic(err)
+		} else if src2 := ast2.JSString(); src != src2 {
+			fmt.Println("JS1:", src)
+			fmt.Println("JS2:", src2)
+			panic("ASTs not equal")
+		}
+		return 1
+	}
+	return 0
+}
